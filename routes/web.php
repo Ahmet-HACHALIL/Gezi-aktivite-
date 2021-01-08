db86bd960493125cde4362846bae9e087fc8d931
Ahmet-HACHALIL <ahmethachalil2014@gmail.com>
2021-01-09 02:51:39 +0300

    Routing Controller View Kullandım

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home1', function () {
     return view('welcome');
 });
+Route::redirect('/anasayfa', '/home')->name('anasayfa');
+
+Route::get('/', function () {
+    return view('home.index');
+});
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+//Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->where(['id'=>'[0-9]+','name'=>'[A-Za-z]+']);
+Route::get('/test/{id}/{name}', [HomeController::class, 'test'])->whereNumber('id')->whereAlpha('name')->name('test');
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
