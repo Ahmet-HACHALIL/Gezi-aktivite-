1d82e088c4889d8d160096e26830223dc8ee7bcc
Ahmet-HACHALIL <ahmethachalil2014@gmail.com>
2021-01-24 17:18:51 +0300

    Setting Site Ayarları ayarladım

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,11 @@ Route::middleware('auth')->prefix('admin')->group(function (){
         Route::get('delete/{id}/{sightseeing_places_id}', [\App\Http\Controllers\Admin\ImageController::class, 'destroy'])->name('admin_image_delete');
         Route::get('show', [\App\Http\Controllers\Admin\ImageController::class, 'show'])->name('admin_image_show');
     });
+
+    #Setting
+    Route::get('setting', [\App\Http\Controllers\Admin\SettingController::class, 'index'])->name('admin_setting');
+    Route::post('setting/update', [\App\Http\Controllers\Admin\SettingController::class, 'update'])->name('admin_setting_update');
+
 });
 
 
